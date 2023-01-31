93805ba31c328c216f2a37ec37f85cb2d5aa1963
Wen KuKu <Anusoesidalavilay@gmail.com>
2023-01-31 09:11:42 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-red-500">My First Vue App</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style  scoped>
+</style>
